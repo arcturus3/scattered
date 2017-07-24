aab978639b141aa59f36d3a58ab976a6efb69e22
Arti Schmidt <artischmidt3@gmail.com>
2017-07-23 22:14:42 -0400

    Made "Use defaults" button work

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsManager : MonoBehaviour {
+
+	public GameObject BallCount;
+	public GameObject BallSpeed;
+	public GameObject GameTime;
+	public GameManager gameManager;
+
+	void Start() {
+		gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+	}
+	public void UseDefaults() {
+		BallCount.transform.GetChild(0).gameObject.GetComponent<Slider>().value = 20;
+		BallSpeed.transform.GetChild(0).gameObject.GetComponent<Slider>().value = 5; 
+		GameTime.transform.GetChild(0).gameObject.GetComponent<Slider>().value = 30;
+	}
+}
