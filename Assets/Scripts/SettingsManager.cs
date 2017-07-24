e6a6eff182f959d85c987b5db8143a18aa12e73e
Arti Schmidt <artischmidt3@gmail.com>
2017-07-24 09:20:14 -0400

    Added functionality to all settings menu options

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -17,5 +17,23 @@ public class SettingsManager : MonoBehaviour {
 		BallCount.transform.GetChild(0).gameObject.GetComponent<Slider>().value = 20;
 		BallSpeed.transform.GetChild(0).gameObject.GetComponent<Slider>().value = 5; 
 		GameTime.transform.GetChild(0).gameObject.GetComponent<Slider>().value = 30;
+		gameManager.ballCount = 20;
+		gameManager.ballSpeed = 5;
+		gameManager.gameTime = 30;
+	}
+
+	public void SetBallCount() {
+		gameManager.ballCount = (int) BallCount.transform.GetChild(0).gameObject.GetComponent<Slider>().value;
+		BallCount.transform.GetChild(2).gameObject.GetComponent<Text>().text = gameManager.ballCount.ToString();
+	}
+
+	public void SetBallSpeed() {
+		gameManager.ballSpeed = (int) BallSpeed.transform.GetChild(0).gameObject.GetComponent<Slider>().value;
+		BallSpeed.transform.GetChild(2).gameObject.GetComponent<Text>().text = gameManager.ballSpeed.ToString();		
+	}
+
+	public void SetGameTime() {
+		gameManager.gameTime = (int) GameTime.transform.GetChild(0).gameObject.GetComponent<Slider>().value;
+		GameTime.transform.GetChild(2).gameObject.GetComponent<Text>().text = gameManager.gameTime.ToString() + " seconds";		
 	}
 }
