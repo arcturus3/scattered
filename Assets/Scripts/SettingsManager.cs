c7a071ae0e28ba65bfe6ebdf4de9cf64ea2cf970
Arti Schmidt <artischmidt3@gmail.com>
2017-07-29 12:04:48 -0400

    Using PlayerPrefs to save settings between loads

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -12,28 +12,60 @@ public class SettingsManager : MonoBehaviour {
 
 	void Start() {
 		gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+		if (!PlayerPrefs.HasKey("BallCount")) {
+			PlayerPrefs.SetInt("BallCount", 4);
+			Debug.Log("set ballCount 4");
+		}
+		if (!PlayerPrefs.HasKey("BallSpeed")) {
+			PlayerPrefs.SetInt("BallSpeed", 5);
+			Debug.Log("set ballSpeed 5");
+		}
+		if (!PlayerPrefs.HasKey("GameTime")) {
+			PlayerPrefs.SetInt("GameTime", 6);
+			Debug.Log("set gameTime 6");
+		}
+
+		BallCount.transform.GetChild(0).gameObject.GetComponent<Slider>().value = PlayerPrefs.GetInt("BallCount");
+		BallSpeed.transform.GetChild(0).gameObject.GetComponent<Slider>().value = PlayerPrefs.GetInt("BallSpeed"); 
+		GameTime.transform.GetChild(0).gameObject.GetComponent<Slider>().value = PlayerPrefs.GetInt("GameTime");
+
+		SetBallCount();
+		SetBallSpeed();
+		SetGameTime();
+
+		gameObject.SetActive(false);
 	}
+
 	public void UseDefaults() {
 		BallCount.transform.GetChild(0).gameObject.GetComponent<Slider>().value = 4;
 		BallSpeed.transform.GetChild(0).gameObject.GetComponent<Slider>().value = 5; 
 		GameTime.transform.GetChild(0).gameObject.GetComponent<Slider>().value = 6;
+
 		gameManager.ballCount = 20;
 		gameManager.ballSpeed = 5;
 		gameManager.gameTime = 30;
+
+		PlayerPrefs.SetInt("BallCount", 4);
+		PlayerPrefs.SetInt("BallSpeed", 5);
+		PlayerPrefs.SetInt("GameTime", 6);
 	}
 
 	public void SetBallCount() {
 		gameManager.ballCount = (int) BallCount.transform.GetChild(0).gameObject.GetComponent<Slider>().value * 5;
 		BallCount.transform.GetChild(2).gameObject.GetComponent<Text>().text = gameManager.ballCount.ToString();
+		PlayerPrefs.SetInt("BallCount", gameManager.ballCount / 5);
 	}
 
 	public void SetBallSpeed() {
 		gameManager.ballSpeed = (int) BallSpeed.transform.GetChild(0).gameObject.GetComponent<Slider>().value;
-		BallSpeed.transform.GetChild(2).gameObject.GetComponent<Text>().text = gameManager.ballSpeed.ToString();		
+		BallSpeed.transform.GetChild(2).gameObject.GetComponent<Text>().text = gameManager.ballSpeed.ToString();
+		PlayerPrefs.SetInt("BallSpeed", gameManager.ballSpeed);		
 	}
 
 	public void SetGameTime() {
 		gameManager.gameTime = (int) GameTime.transform.GetChild(0).gameObject.GetComponent<Slider>().value * 5;
-		GameTime.transform.GetChild(2).gameObject.GetComponent<Text>().text = gameManager.gameTime.ToString() + " <size=30>seconds</size>";		
+		GameTime.transform.GetChild(2).gameObject.GetComponent<Text>().text = gameManager.gameTime.ToString() + " <size=30>seconds</size>";
+		PlayerPrefs.SetInt("GameTime", gameManager.gameTime / 5);
 	}
 }
