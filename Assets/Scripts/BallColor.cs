1460d92addd6d0c45d09c355de45001a30b95af1
Arti Schmidt <artiiii@thefarmhouse.com>
2017-08-23 17:03:42 -0400

    Added credits and some shop components

---
+++ Assets/Scripts/BallColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallColor : ScriptableObject {
+
+	public int ID {get;};
+	public Color Value {get;};
+	public bool Unlocked {get; set;} = false;
+	public bool Selected {get; set;} = false;
+
+	public BallColor(int id, Color value) {
+		ID = id;
+		Value = value;
+	}
+}
